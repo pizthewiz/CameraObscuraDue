f0d257a193200d165bfb53e7ed28ab5d53254617
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2012-10-01 20:03:01 -0700

    initial commit of skeleton project

---
+++ CameraObscuraDue/CameraObscuraDuePlugIn.h
@@ -0,0 +1,12 @@
+//
+//  CameraObscuraDuePlugIn.h
+//  CameraObscuraDue
+//
+//  Created by Jean-Pierre Mouilleseaux on 29 Sept 2012.
+//  Copyright (c) 2012 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface CameraObscuraDuePlugIn : QCPlugIn
+@end
